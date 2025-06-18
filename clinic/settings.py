58a5463d09bfbb5498f6812a13a48125f3047b4f
acidqw <im.acidacidacid@gmail.com>
2025-06-18 21:21:57 +0300

    27

--- clinic/settings.py
+++ clinic/settings.py
@@ -127,10 +127,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 if os.environ.get("VERCEL"):
 
 
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+    
 
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
