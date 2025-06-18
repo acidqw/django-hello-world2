e1ed2911dc045d8083ee710f6a0332b1acfa68dc
acidqw <im.acidacidacid@gmail.com>
2025-06-18 19:59:48 +0300

    5

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ python3.9 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
-python3 manage.py collectstatic --no-input
+python3 manage.py collectstatic
+python3 manage.py runserver
